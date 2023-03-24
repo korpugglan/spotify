40fe397c47d6eecf4cde8610f5018ab06b4211b6
korpugglan <korpugglan@hotmail.com>
2023-03-24 15:34:39 +0100

    Finalize first OAuth step with correct localhost port

--- friday.py
+++ friday.py
@@ -7,7 +7,11 @@
 # TODO: Remove missing from target playlist
 
 # Import packages
+import base64
+import hashlib
 import json
+import requests_oauthlib as oauthlib
+import uuid
 
 
 # Define functions
@@ -39,7 +43,7 @@ def print_line(print_chars="-", repetition=100):
 # Define global variables
 api_dict = {"base_url": "https://api.spotify.com/v1",
             "auth_url": "https://accounts.spotify.com/authorize",
-            "redirect_uri": "http://localhost:8080",
+            "redirect_uri": "http://localhost:6942",
             "endpoints": {
                 "auth": "/authorization"
                 }
@@ -53,8 +57,19 @@ if __name__ == "__main__":
     # print(cred_dict)
 
     print("RUNNING USER AUTHORIZATION")
+    rfc_state = uuid.uuid4().hex
+    code_verifier = base64.urlsafe_b64encode(uuid.uuid4().hex.encode("utf-8"))
+    challenge_bytes = hashlib.sha256(code_verifier).digest()
+    code_challenge = base64.urlsafe_b64encode(challenge_bytes).rstrip(b'=')
+
+    oauth = oauthlib.OAuth2Session(cred_dict["client_id"], redirect_uri=api_dict["redirect_uri"])  # , scope=scope)
+    auth_url, state = oauth.authorization_url(api_dict["auth_url"])
+    print(f"Please go to {auth_url} and authorize access.")
+    auth_response = input("Enter the full callback URL: ")
+
+    print_line()
+    print(auth_response)
 
     print_line("=")
     print("Ciao bella, ciao")
     print_line("=")
-    # sys.exit()
