dc1ec07986018b0e949216e1da5750ecd5c3ac59
korpugglan <korpugglan@hotmail.com>
2023-03-24 14:55:00 +0100

    Move OAuth 2.0 Authorization flow with PKCE extension to temp.py and add that file to repo

--- friday.py
+++ friday.py
@@ -7,14 +7,7 @@
 # TODO: Remove missing from target playlist
 
 # Import packages
-import base64
-import hashlib
-from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-import requests
-import requests_oauthlib as oauthlib
-import sys
-import uuid
 
 
 # Define functions
@@ -47,8 +40,6 @@ def print_line(print_chars="-", repetition=100):
 api_dict = {"base_url": "https://api.spotify.com/v1",
             "auth_url": "https://accounts.spotify.com/authorize",
             "redirect_uri": "http://localhost:8080",
-            # "redirect_uri": "https://localhost:8888/callback",
-            # "redirect_uri": "https://open.spotify.com/collection/playlists",
             "endpoints": {
                 "auth": "/authorization"
                 }
@@ -62,38 +53,6 @@ if __name__ == "__main__":
     # print(cred_dict)
 
     print("RUNNING USER AUTHORIZATION")
-    # https://developer.spotify.com/documentation/general/guides/authorization/code-flow/
-    # print(api_dict)
-    rfc_state = uuid.uuid4().hex
-    code_verifier = base64.urlsafe_b64encode(uuid.uuid4().hex.encode("utf-8"))
-    challenge_bytes = hashlib.sha256(code_verifier).digest()
-    code_challenge = base64.urlsafe_b64encode(challenge_bytes).rstrip(b'=')
-
-    # TODO: fix and set up redirect uri locally (?)
-    # payload = {"client_id": cred_dict["client_id"],
-    #            "response_type": "code",
-    #            "redirect_uri": api_dict["redirect_uri"],
-    #            "state": rfc_state,
-    #            # "scope": "https://developer.spotify.com/documentation/general/guides/authorization/scopes/",
-    #            # "show_dialog": "false",
-    #            "code_challenge_method": "S256",
-    #            "code_challenge": code_challenge
-    #            }
-    #
-    # auth_response = requests.get(api_dict["auth_url"], params=payload)
-
-
-    local_server = HTTPServer(("localhost", 8080), BaseHTTPRequestHandler)
-
-    oauth = oauthlib.OAuth2Session(cred_dict["client_id"], redirect_uri=api_dict["redirect_uri"])  # , scope=scope)
-    auth_url, state = oauth.authorization_url(api_dict["auth_url"])
-    print(f"Please go to {auth_url} and authorize access.")
-    auth_response = input("Enter the full callback URL: ")
-
-    print_line()
-    print(auth_response)
-
-    local_server.server_close()
 
     print_line("=")
     print("Ciao bella, ciao")
---
+++ temp.py
@@ -0,0 +1,48 @@
+# Dump for parts of code to reuse later on
+
+import base64
+import hashlib
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import requests
+import requests_oauthlib as oauthlib
+import sys
+import uuid
+
+api_dict = {"base_url": "https://api.spotify.com/v1",
+            "auth_url": "https://accounts.spotify.com/authorize",
+            "redirect_uri": "http://localhost:8080",
+            "endpoints": {
+                "auth": "/authorization"
+                }
+            }
+
+# https://developer.spotify.com/documentation/general/guides/authorization/code-flow/
+# print(api_dict)
+rfc_state = uuid.uuid4().hex
+code_verifier = base64.urlsafe_b64encode(uuid.uuid4().hex.encode("utf-8"))
+challenge_bytes = hashlib.sha256(code_verifier).digest()
+code_challenge = base64.urlsafe_b64encode(challenge_bytes).rstrip(b'=')
+
+# payload = {"client_id": cred_dict["client_id"],
+#            "response_type": "code",
+#            "redirect_uri": api_dict["redirect_uri"],
+#            "state": rfc_state,
+#            # "scope": "https://developer.spotify.com/documentation/general/guides/authorization/scopes/",
+#            # "show_dialog": "false",
+#            "code_challenge_method": "S256",
+#            "code_challenge": code_challenge
+#            }
+#
+# auth_response = requests.get(api_dict["auth_url"], params=payload)
+
+
+# local_server = HTTPServer(("localhost", 8080), BaseHTTPRequestHandler)
+
+oauth = oauthlib.OAuth2Session(cred_dict["client_id"], redirect_uri=api_dict["redirect_uri"])  # , scope=scope)
+auth_url, state = oauth.authorization_url(api_dict["auth_url"])
+print(f"Please go to {auth_url} and authorize access.")
+auth_response = input("Enter the full callback URL: ")
+print(auth_response)
+
+# local_server.server_close()
